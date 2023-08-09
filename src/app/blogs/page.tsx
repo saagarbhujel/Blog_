c45382267cee8ea49a1ee36b7c8370a3a84e20c5
saagarbhujel <saagarbhujel22@gmail.com>
2023-08-09 22:27:06 +0545

    blog data fetch and populate in blog page

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,13 +1,41 @@
-import React from "react";
+'use client'
+import React,{useEffect, useState} from "react";
 import Link from "next/link";
+import axios from "axios";
+import uniqid from 'uniqid';
+
 
 
 const Blogs = () => {
+  const [blogs, setBlogs] = useState([])
+
+
+  const fetchedBlog = async() =>{
+try {
+    const response = await axios.get('http://localhost:3000/api/users/blogpost')
+    // console.log(response);
+    setBlogs(response.data.allBlogData)
+    // console.log(response.data.allBlogData)
+} catch (error) {
+  console.log(error);
+  
+}
+ 
+  }
+
+ useEffect(() => {
+fetchedBlog()
+ }, [])
+ 
+  // console.log(uniqid)
   return (
     <>
-      <h2 className=" text-center text-6xl underline mt-12 mb-12">Blogs</h2>
-      <div className="md:grid grid-cols-3  content-center lg:grid-cols-4 justify-items-center mt-8 mb-8 ">
-        <Link href="/blogs/learnNext">
+    <h2 className=" text-center text-6xl underline mt-12 mb-12">Blogs</h2>
+    <div className="md:grid grid-cols-3  content-center lg:grid-cols-4 justify-items-center mt-8 mb-8 ">
+  {blogs.map((blog,index) =>{
+    return (
+   
+        <Link  key={uniqid()} href="/blogs/learnNext">
           <div className=" max-w-sm border border-solid border-black mt-8  transition ease-in-out delay-15 hover:-translate-y-1 hover:scale-110 duration-300 rounded-[5px]">
             <div className="">
               <img
@@ -17,20 +45,30 @@ const Blogs = () => {
             </div>
             <div className="p-2">
               <div>
-                <h3 className=" text-[20px] font-semibold">Title</h3>
+                <h3 className=" text-[20px] font-semibold">{blog.title}</h3>
               </div>
               <div>
                 <p>
-                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                  Deserunt amet qui consequuntur velit earum.
+                 {blog.description}
                 </p>
               </div>
             </div>
           </div>
         </Link>
+  
+    )
+  })}
       </div>
+
+  
     </>
   );
 };
 
 export default Blogs;
+
+
+
+{/* <div key={index}>
+      <h1>{blog.title}</h1>
+     </div> */}
