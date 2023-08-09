7807f3c68db005885d2820f2069fa25bb47626c4
saagarbhujel <saagarbhujel22@gmail.com>
2023-08-09 17:32:15 +0545

    all blog data send to the endpoint

--- src/app/api/users/blogpost/route.ts
+++ src/app/api/users/blogpost/route.ts
@@ -1,12 +1,23 @@
 import * as fs from "fs";//this is import for accessing local json files
-import { NextResponse, NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 
-export function GET(response: NextResponse, request: NextRequest) {
-  try {
-    const data = fs.readdirSync("blog-data"); //accessing json file 
-   console.log(data);
-   return NextResponse.json({data, success: true});
-  } catch (error) {
-    return NextResponse.json({ message: "error", success: false });
-  }
+export async function GET(response: NextResponse) {
+
+    const data =  fs.readdirSync("blog-data"); //accessing json file 
+  //  console.log(data);
+   const allBlogData = []
+
+   for (let index = 0; index < data.length; index++) {
+    const item = data[index];
+    // console.log(item);
+    const fetch = fs.readFileSync(("blog-data/" + item), "utf8");
+    // console.log(fetch)
+    allBlogData.push(JSON.parse(fetch));
+    
+    
+   }
+   return NextResponse.json({allBlogData, success: true});
+
+
+ 
 }
