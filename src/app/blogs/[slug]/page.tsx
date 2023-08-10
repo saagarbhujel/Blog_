a30181afb54c6b690db609cabea74eb6923d619b
saagarbhujel <saagarbhujel22@gmail.com>
2023-08-10 23:15:03 +0545

    data fetch from endpoint, present in dynamic route

--- src/app/blogs/[slug]/page.tsx
+++ src/app/blogs/[slug]/page.tsx
@@ -1,26 +1,39 @@
-import React from "react";
+'use client'
+
+import axios from "axios";
+
+
+import React,{useState, useEffect} from "react";
 
 const Page = ({ params }: { params: { slug: string } }) => {
+  const [blog, setBlog] = useState({
+    title:'',
+    content:''
+  })
+
+  const fetchedBlogitem = async()=>{
+  try {
+    const response = await axios.get(`http://localhost:3000/api/users/getblog?slug=${params.slug}`)
+    // console.log(response.data);
+    setBlog(response.data)
+  } catch (error:any) {
+    console.log(error.message)
+  }
+  }
+  useEffect(()=>{
+    fetchedBlogitem()
+  },[])
   return (
     <>
       <div className="relative max-w-screen-xl px-4 py-10 mx-auto md:flex md:py-10 gap-x-6 md:flex-row">
         <article className=" m-[0 auto]  ">
           <h1 className=" text-[clamp(28px,5vw,48px)!important] text-center mb-[48px] underline underline-offset-8">
-            Title:{params.slug}
+            Title: {blog && blog.title}
           </h1>
           <hr />
           <div className="mt-[48px]">
             <p className="pl-6 pr-6">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit
-              dicta repellendus tenetur, laboriosam assumenda modi blanditiis
-              aliquam eaque est vitae quasi inventore illum ab harum
-              consequuntur sequi, itaque nihil eligendi! Lorem ipsum dolor sit
-              amet, consectetur adipisicing elit. Deleniti fugit harum
-              recusandae illum rerum quos temporibus, repellendus accusantium
-              ratione, suscipit autem ullam pariatur fugiat a veniam delectus
-              dolorum. Dolore, illum. Saepe incidunt, consequatur laudantium
-              cumque autem ut adipisci sint quas aut voluptate laborum facere
-              accusantium.
+              {blog && blog.content}
             </p>
           </div>
         </article>
