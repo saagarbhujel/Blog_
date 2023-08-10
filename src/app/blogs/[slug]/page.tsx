924420146ca614358d1221303fe80a37ed8bdb0e
saagarbhujel <saagarbhujel22@gmail.com>
2023-08-10 23:41:34 +0545

    image added to blog

--- src/app/blogs/[slug]/page.tsx
+++ src/app/blogs/[slug]/page.tsx
@@ -1,40 +1,43 @@
-'use client'
+"use client";
 
 import axios from "axios";
 
-
-import React,{useState, useEffect} from "react";
-
+import React, { useState, useEffect } from "react";
+import Image from "next/image";
 const Page = ({ params }: { params: { slug: string } }) => {
   const [blog, setBlog] = useState({
-    title:'',
-    content:''
-  })
+    title: "",
+    content: "",
+  });
 
-  const fetchedBlogitem = async()=>{
-  try {
-    const response = await axios.get(`http://localhost:3000/api/users/getblog?slug=${params.slug}`)
-    // console.log(response.data);
-    setBlog(response.data)
-  } catch (error:any) {
-    console.log(error.message)
-  }
-  }
-  useEffect(()=>{
-    fetchedBlogitem()
-  },[])
+  const fetchedBlogitem = async () => {
+    try {
+      const response = await axios.get(
+        `http://localhost:3000/api/users/getblog?slug=${params.slug}`
+      );
+      // console.log(response.data);
+      setBlog(response.data);
+    } catch (error: any) {
+      console.log(error.message);
+    }
+  };
+  useEffect(() => {
+    fetchedBlogitem();
+  }, []);
   return (
     <>
       <div className="relative max-w-screen-xl px-4 py-10 mx-auto md:flex md:py-10 gap-x-6 md:flex-row">
         <article className=" m-[0 auto]  ">
           <h1 className=" text-[clamp(28px,5vw,48px)!important] text-center mb-[48px] underline underline-offset-8">
-            Title: {blog && blog.title}
+             {blog && blog.title}
           </h1>
           <hr />
+          <Image className=" m-auto mt-10" width={500}
+                    height={500}
+                    src="/code-background.avif"
+                    alt="coding img"/>
           <div className="mt-[48px]">
-            <p className="pl-6 pr-6">
-              {blog && blog.content}
-            </p>
+            <p className="pl-6 pr-6">{blog && blog.content}</p>
           </div>
         </article>
       </div>
