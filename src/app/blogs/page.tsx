c1645381b975a54e3c89ffa546036a2b89ed88c6
saagarbhujel <saagarbhujel22@gmail.com>
2023-08-10 23:41:08 +0545

    image component used instead of img

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,74 +1,68 @@
-'use client'
-import React,{useEffect, useState} from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import axios from "axios";
-import uniqid from 'uniqid';
-
-
+import uniqid from "uniqid";
+import Image from "next/image";
 
 const Blogs = () => {
-  const [blogs, setBlogs] = useState([])
+  const [blogs, setBlogs] = useState([]);
 
+  const fetchedBlog = async () => {
+    try {
+      const response = await axios.get(
+        "http://localhost:3000/api/users/blogpost"
+      );
+      // console.log(response);
+      setBlogs(response.data.allBlogData);
+      // console.log(response.data.allBlogData)
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-  const fetchedBlog = async() =>{
-try {
-    const response = await axios.get('http://localhost:3000/api/users/blogpost')
-    // console.log(response);
-    setBlogs(response.data.allBlogData)
-    // console.log(response.data.allBlogData)
-} catch (error) {
-  console.log(error);
-  
-}
- 
-  }
+  useEffect(() => {
+    fetchedBlog();
+  }, []);
 
- useEffect(() => {
-fetchedBlog()
- }, [])
- 
   // console.log(uniqid)
   return (
     <>
-    <h2 className=" text-center text-6xl underline mt-12 mb-12">Blogs</h2>
-    <div className="md:grid grid-cols-3  content-center lg:grid-cols-4 justify-items-center mt-8 mb-8 ">
-  {blogs.map((blog,index) =>{
-    return (
-   
-        <Link  key={uniqid()} href={`/blogs/${blog.slug}`}>
-          <div className=" max-w-sm border border-solid border-black mt-8  transition ease-in-out delay-15 hover:-translate-y-1 hover:scale-110 duration-300 rounded-[5px]">
-            <div className="">
-              <img
-                src="https://imgs.search.brave.com/eL6922PWuxZt2YKIRPaz4z3YNveKpjAje28SE_7TGvA/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/cHJlbWl1bS1waG90/by9kZXNrdG9wLXNv/dXJjZS1jb2RlLXdh/bGxwYXBlci1ieS1j/b21wdXRlci1sYW5n/dWFnZS13aXRoLWNv/ZGluZy1wcm9ncmFt/bWluZ18zMzc3MS01/OTUuanBnP3NpemU9/NjI2JmV4dD1qcGc"
-                alt="coding img"
-              />
-            </div>
-            <div className="p-2">
-              <div>
-                <h3 className=" text-[20px] font-semibold">{blog.title}</h3>
+      <h2 className=" text-center text-6xl underline mt-12 mb-12">Blogs</h2>
+      <div className="md:grid grid-cols-3  content-center lg:grid-cols-4 justify-items-center mt-8 mb-8 ">
+        {blogs.map((blog, index) => {
+          return (
+            <Link key={uniqid()} href={`/blogs/${blog.slug}`}>
+              <div className=" max-w-sm border border-solid border-black mt-8  transition ease-in-out delay-15 hover:-translate-y-1 hover:scale-110 duration-300 rounded-[5px]">
+                <div className="">
+                  <Image
+                    width={500}
+                    height={500}
+                    src="/code-background.avif"
+                    alt="coding img"
+                  />
+                </div>
+                <div className="p-2">
+                  <div>
+                    <h3 className=" text-[20px] font-semibold">{blog.title}</h3>
+                  </div>
+                  <div>
+                    <p>{blog.description}</p>
+                  </div>
+                </div>
               </div>
-              <div>
-                <p>
-                 {blog.description}
-                </p>
-              </div>
-            </div>
-          </div>
-        </Link>
-  
-    )
-  })}
+            </Link>
+          );
+        })}
       </div>
-
-  
     </>
   );
 };
 
 export default Blogs;
 
-
-
-{/* <div key={index}>
+{
+  /* <div key={index}>
       <h1>{blog.title}</h1>
-     </div> */}
+     </div> */
+}
