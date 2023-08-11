bc2bffa06d5677558b472e9aca75d9c63eb9a995
saagarbhujel <saagarbhujel22@gmail.com>
2023-08-11 17:35:26 +0545

    modified to server side rendering and type defined

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,39 +1,42 @@
-"use client";
-import React, { useEffect, useState } from "react";
+
+import React from "react";
 import Link from "next/link";
-import axios from "axios";
+import axios,{AxiosError} from "axios";
 import uniqid from "uniqid";
 import Image from "next/image";
 
-const Blogs = () => {
-  const [blogs, setBlogs] = useState([]);
+type Blog = {
+  slug: string;
+  title: string;
+  description: string;
+  content: string;
+};
+ const fetchBlog=async()=> {
+try {
+   const response = await axios.get("http://localhost:3000/api/users/blogpost");
 
-  const fetchedBlog = async () => {
-    try {
-      const response = await axios.get(
-        "http://localhost:3000/api/users/blogpost"
-      );
-      // console.log(response);
-      setBlogs(response.data.allBlogData);
-      // console.log(response.data.allBlogData)
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  //  console.log(response.data);
+   return response.data.allBlogData;
+} catch (err) {
+  const error = err as AxiosError;
+  return error.message;
+}
+}
+ 
+const Blogs = async() => {
+  const blogs = await fetchBlog()
 
-  useEffect(() => {
-    fetchedBlog();
-  }, []);
-
-  // console.log(uniqid)
+  
+  
   return (
     <>
+ 
       <h2 className=" text-center text-6xl underline mt-12 mb-12">Blogs</h2>
-      <div className="md:grid grid-cols-3  content-center lg:grid-cols-4 justify-items-center mt-8 mb-8 ">
-        {blogs.map((blog, index) => {
+      <div className=" grid md:grid-cols-2  content-center lg:grid-cols-3 xl:grid-cols-4 justify-items-center mt-8 mb-8 ">
+        {blogs.map((blog:Blog) => {
           return (
             <Link key={uniqid()} href={`/blogs/${blog.slug}`}>
-              <div className=" max-w-sm border border-solid border-black mt-8  transition ease-in-out delay-15 hover:-translate-y-1 hover:scale-110 duration-300 rounded-[5px]">
+              <div className=" max-w-[20rem] lg:max-w-[18rem] border border-solid border-black mt-8  transition ease-in-out delay-15 md:hover:-translate-y-1 hover:scale-110 duration-300 rounded-[5px]">
                 <div className="">
                   <Image
                     width={500}
@@ -51,18 +54,15 @@ const Blogs = () => {
                   </div>
                 </div>
               </div>
-            </Link>
-          );
+            </Link> 
+           );
         })}
       </div>
     </>
   );
 };
 
+
+
 export default Blogs;
 
-{
-  /* <div key={index}>
-      <h1>{blog.title}</h1>
-     </div> */
-}
