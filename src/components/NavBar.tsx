42796044d444adbcbae6c41ceef410679e105541
saagarbhujel <saagarbhujel22@gmail.com>
2023-08-11 19:17:59 +0545

    hover added to navitem & close open nav modified

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,66 +4,87 @@ import React from "react";
 import Link from "next/link";
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
+
 const NavBar = () => {
   const [mobileNav, setMobileNav] = React.useState(true);
 
-  const handleHam = () => {
-    setMobileNav(!mobileNav);
+  const handleHam = (e:any) => {
+ setMobileNav(!mobileNav)
   };
+  
   return (
     <>
       {/* For bigger devices */}
-      <nav className=" top-0 w-full flex justify-between h-[71px] mx-w-[1240px] mx-auto px-4 bg-white text-black py-3 text-lg z-10">
-  <h1 className="text-3xl font-bold text-[0000] flex items-center">
-    Blog
-  </h1>
-  <div className="nav-list flex justify-center items-center flex-grow">
-    <ul className="hidden md:flex justify-center px-4 gap-[4vw] uppercase">
-      <Link href="/">
-        <li className="cursor-pointer text-[20px] ">
-          Home
-        </li>
-      </Link>
-      <Link href="/about">
-        <li className="cursor-pointer text-[20px] ">
-          About
-        </li>
-      </Link>
-      <Link href="/blogs">
-        <li className="cursor-pointer text-[20px] ">
-          Blogs
-        </li>
-      </Link>
-      <Link href="/contact">
-        <li className="cursor-pointer text-[20px] ">
-          Contact
-        </li>
-      </Link>
-    </ul>
-  </div>
+      <nav className="fixed top-0 w-full flex justify-between h-[71px]  mx-w-[1240px] mx-auto px-4 bg-white text-black py-3 text-lg z-10">
+        <h1 className="text-3xl font-bold text-[0000] flex items-center">
+          Blog
+        </h1>
+        <div className="nav-list flex justify-center items-center flex-grow">
+          <ul className="hidden md:flex justify-center px-4 gap-[4vw] uppercase">
+            <Link href="/">
+              <li className="cursor-pointer underline underline-offset-8 hover:no-underline  text-[20px] transition ease-in-out delay-15 md:hover:-translate-y-1 hover:scale-105 duration-300">
+                Home
+              </li>
+            </Link>
+            <Link href="/about">
+              <li className="cursor-pointer underline underline-offset-8 hover:no-underline  text-[20px] transition ease-in-out delay-15 md:hover:-translate-y-1 hover:scale-105 duration-300  ">
+                About
+              </li>
+            </Link>
+            <Link href="/blogs">
+              <li className="cursor-pointer underline underline-offset-8 hover:no-underline text-[20px] transition ease-in-out delay-15 md:hover:-translate-y-1 hover:scale-105 duration-300 ">
+                Blogs
+              </li>
+            </Link>
+            <Link href="/contact">
+              <li className="cursor-pointer underline underline-offset-8 hover:no-underline text-[20px] transition ease-in-out delay-15 md:hover:-translate-y-1 hover:scale-105 duration-300 ">
+                Contact
+              </li>
+            </Link>
+          </ul>
+        </div>
         {/* for smaller devices */}
-        <div onClick={handleHam} className="flex  items-center md:hidden">
-          {!mobileNav ? (
-            <AiOutlineClose size={30} />
-          ) : (
-            <AiOutlineMenu size={30} />
-          )}
+
+        <div className="flex  items-center md:hidden">
+          <div onClick={handleHam}>
+            {!mobileNav ? (
+              <AiOutlineClose hidden size={30} />
+            ) : (
+              <AiOutlineMenu className=" cursor-pointer" size={30} />
+            )}
+          </div>
 
           <div
             className={
               !mobileNav
-                ? "nav-list fixed left-0 top-[60px] w-[60%] h-full z-0 border-r border-r-purple-900 bg-gray-800  ease-linear duration-300 md:hidden"
+                ? "nav-list fixed left-0 top-0 w-[60%] h-full z-0 border-r  bg-gray-700  ease-linear duration-300 md:hidden"
                 : "fixed left-[-100%]"
             }
           >
-            <ul className="  text-white uppercase text-center ">
+            <div className="flex justify-between items-center mt-4 mb-4 ml-2 mr-2">
+              <h1 className=" text-white text-3xl font-bold text-[0000] cursor-default">
+                Blogs
+              </h1>
+              <div onClick={handleHam}>
+                <AiOutlineClose
+                  className="text-white cursor-pointer"
+                  size={30}
+                />
+              </div>
+            </div>
+            <hr />
+
+            <ul className=" mt-14 text-white uppercase text-center ">
               <Link href="/">
-               
                 <li className=" p-4">Home</li>
               </Link>
               <Link href="/about">
                 <li className=" p-4">About</li>
               </Link>
+              <Link href="/blogs">
+                <li className="p-4 ">Blogs</li>
+              </Link>
+
               <Link href="/contact">
                 <li className=" p-4">Contact</li>
               </Link>
