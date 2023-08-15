a32678c332019b25183a0af45bdfbbbbdb78aced
saagarbhujel <saagarbhujel22@gmail.com>
2023-08-15 22:45:35 +0545

    latestblog component made and blog data fetched

---
+++ src/components/LatestBlog.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import Link from "next/link";
+import axios, { AxiosError } from "axios";
+import uniqid from "uniqid";
+import Image from "next/image";
+
+type Blog = {
+  slug: string;
+  title: string;
+  description: string;
+  content: string;
+};
+
+const fetchBlog = async () => {
+  try {
+    const response = await axios.get(
+      "http://localhost:3000/api/users/blogpost"
+    );
+    // console.log(response.data);
+    return response.data.allBlogData;
+  } catch (err) {
+    const error = err as AxiosError;
+    return error.message;
+  }
+};
+const LatestBlog = async () => {
+  const blogs = await fetchBlog();
+  const latestBlogs = blogs.slice(0, 3);
+  return (
+    <>
+      <h2 className="text-center text-4xl underline  mt-6 mb-6 cursor-default">
+        Popular Blogs
+      </h2>
+      <section className="flex items-center justify-center gap-[7rem] mt-4 mb-12 ">
+        {latestBlogs.map((blog: Blog) => {
+          return (
+            <Link key={uniqid()} href={`/blogs/${blog.slug}`}>
+              <div className=" max-w-[20rem] lg:max-w-[18rem] xl:max-w-[20rem] 2xl:max-w-[25rem] border border-solid border-black mt-8  transition ease-in-out delay-15 md:hover:-translate-y-1 hover:scale-105 duration-300 rounded-[5px]">
+                <div className="">
+                  <Image
+                    width={500}
+                    height={500}
+                    src="/code-background.avif"
+                    alt="coding img"
+                  />
+                </div>
+                <section className="p-2">
+                  <div>
+                    <h3 className="text-[20px] font-semibold">{blog.title}</h3>
+                  </div>
+                  <div>
+                    <p>{blog.description}</p>
+                  </div>
+                </section>
+              </div>
+            </Link>
+          );
+        })}
+      </section>
+    </>
+  );
+};
+
+export default LatestBlog;
