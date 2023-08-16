f4ea952897f6ad2a268080def052e22bbda5ed1d
saagarbhujel <saagarbhujel22@gmail.com>
2023-08-16 14:58:58 +0545

    button and requires indicator added, h,m adjusted

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -46,16 +46,21 @@ const Contact = () => {
   }
   return (
     <>
-    <div><Toaster/></div>
-      <div className=" min-h-screen flex items-center justify-center">
-        <div className="w-full max-w-md p-6  rounded-[10px] shadow-2xl shadow-slate-400">
-          <h3 className="text-2xl font-semibold mb-4 text-center">Contact Us</h3>
+      <div>
+        <Toaster />
+      </div>
+      <div className="min-h-[90vh]  flex items-center justify-center">
+        <div className=" w-4/5 md:w-full md:max-w-md p-6 mt-16 rounded-[10px] shadow-2xl shadow-slate-400">
+          <h3 className="text-2xl font-semibold mb-4 text-center">
+            Contact Us
+          </h3>
           <form onSubmit={handleSubmit}>
             <div className="mb-4">
               <label
                 htmlFor="name"
                 className="block text-gray-700 font-semibold mb-1"
               >
+                <span className=" text-red-400 font-thin">* </span>
                 Name
               </label>
               <input
@@ -74,6 +79,8 @@ const Contact = () => {
                 htmlFor="email"
                 className="block text-gray-700 font-semibold mb-1"
               >
+                {" "}
+                <span className=" text-red-400 font-thin">* </span>
                 Email
               </label>
               <input
@@ -92,6 +99,7 @@ const Contact = () => {
                 htmlFor="message"
                 className="block text-gray-700 font-semibold mb-1"
               >
+                <span className=" text-red-400 font-thin">* </span>
                 Message
               </label>
               <textarea
@@ -108,7 +116,7 @@ const Contact = () => {
             <div className="text-center">
               <button
                 type="submit"
-                className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 focus:outline-none"
+                className="bg-black text-white px-4 py-2 transition duration-300 ease-in-out rounded-lg hover:bg-white hover:text-black border hover:border-black focus:outline-none"
               >
                 Submit
               </button>
